a2faab7a06892612f3ea567336911f865b967cc2
Alex Walker <AWalker@qa.com>
2021-05-12 16:51:04 +0100

    AW: Results

---
+++ src/com/qa/main/Results.java
@@ -0,0 +1,40 @@
+package com.qa.main;
+
+public class Results {
+	public static int physics;
+	public static int chemistry;
+	public static int biology;
+	public static int total;
+	public static double percentage;
+	
+	
+	public static void method1(int phygrade, int chemgrade, int biograde) {
+		
+		physics = phygrade;
+		chemistry = chemgrade;
+		biology = biograde;
+		method2();
+		
+		System.out.println("You're Physics Grade is: " + physics + ". You're Physics Percentage is: "
+				+ ((physics * 100) / 150) + "%");
+		
+		System.out.println("You're Chemistry Grade is: " + chemistry + ". You're Chemisty Percentage is: "
+				+ ((chemistry * 100) / 150) + "%");
+		
+		System.out.println("You're Biology Grade is: " + biology + ". You're Biology Percentage is: "
+				+ ((biology * 100) / 150) + "%");
+		
+		System.out.println("You're Total Grade is: " + total + ". You're Total Percentage is: "
+				+ percentage + "%");
+		
+	}
+	
+	public static void method2() {
+		
+		total = physics + chemistry + biology;
+		percentage = (total * 100) / 450;
+
+		
+	}
+
+}
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -2,6 +2,11 @@ package com.qa.main;
 
 public class Runner {
 	
-	
+	public static void main(String[] args) {
+		
+		//ResultsRevisited.method1(100, 80, 80);
+		
+		Results.method1(10, 20, 30);
+    }
 
 }
